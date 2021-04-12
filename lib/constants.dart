b40ce810f934d885f75ac5f6fd6d8d706c94782c
nilsonsasaki <nilsonsasaki@gmail.com>
2021-04-12 10:47:30 -0300

    added constant kFloatButtonColor

--- lib/constants.dart
+++ lib/constants.dart
@@ -5,6 +5,7 @@ const Color kInactiveColor = Color(0xFF111328);
 const double kBottomContainerHeight = 80.0;
 const Color kBottomContainerColor = Color(0xFFEB1555);
 const Color kLabelTextColor = Color(0xFF8D8E98);
+const Color kFloatButtonColor = Color(0xFF4c4F5E);
 
 const kLabelTextStyle = TextStyle(
   fontSize: 18.0,
