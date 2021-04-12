1618b6fe024f29a1d78acc1df98e707dee95002b
nilsonsasaki <nilsonsasaki@gmail.com>
2021-04-12 08:41:17 -0300

    added a constants file

---
+++ lib/constants.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+const Color kActiveColor = Color(0xFF1D1E33);
+const Color kInactiveColor = Color(0xFF111328);
+const double kBottomContainerHeight = 80.0;
+const Color kBottomContainerColor = Color(0xFFEB1555);
+const Color kLabelTextColor = Color(0xFF8D8E98);
+
+const kLabelTextStyle = TextStyle(
+  fontSize: 18.0,
+  color: kLabelTextColor,
+);
+
+const kNumberTextStyle = TextStyle(
+  fontSize: 50.0,
+  fontWeight: FontWeight.w900,
+);
