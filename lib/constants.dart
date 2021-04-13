e69550bedbb93b2963107f5967eaf8d23bb361e4
nilsonsasaki <nilsonsasaki@gmail.com>
2021-04-13 14:23:46 -0300

    added constant configurations

--- lib/constants.dart
+++ lib/constants.dart
@@ -6,6 +6,7 @@ const double kBottomContainerHeight = 80.0;
 const Color kBottomContainerColor = Color(0xFFEB1555);
 const Color kLabelTextColor = Color(0xFF8D8E98);
 const Color kFloatButtonColor = Color(0xFF4c4F5E);
+const Color kNormalResultTextColor = Color(0xFF24D876);
 
 const kLabelTextStyle = TextStyle(
   fontSize: 18.0,
@@ -15,4 +16,30 @@ const kLabelTextStyle = TextStyle(
 const kNumberTextStyle = TextStyle(
   fontSize: 50.0,
   fontWeight: FontWeight.w900,
+);
+
+const kLargeButtonTextStyle = TextStyle(
+  fontWeight: FontWeight.bold,
+  fontSize: 24.0,
+);
+
+const kTitleTextStyle = TextStyle(
+  fontWeight: FontWeight.bold,
+  fontSize: 50.0,
+);
+
+const kNormalResultTextStyle = TextStyle(
+  color: kNormalResultTextColor,
+  fontWeight: FontWeight.bold,
+  fontSize: 22.0,
+);
+
+const kResultNumberStyle = TextStyle(
+  fontWeight: FontWeight.bold,
+  fontSize: 100.0,
+);
+
+const kResultBodyTextStyle = TextStyle(
+  fontWeight: FontWeight.bold,
+  fontSize: 22.0,
 );
