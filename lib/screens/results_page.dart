f12123c48f5508a4bfcec3a027e09521d30df32e
nilsonsasaki <nilsonsasaki@gmail.com>
2021-04-15 16:38:04 -0300

    added inputs

--- lib/screens/results_page.dart
+++ lib/screens/results_page.dart
@@ -4,6 +4,15 @@ import '../constants.dart';
 import '../components/bottom_button.dart';
 
 class ResultsPage extends StatelessWidget {
+  ResultsPage(
+      {@required this.bmiResult,
+      @required this.resultText,
+      @required this.interpretation});
+
+  final String bmiResult;
+  final String resultText;
+  final String interpretation;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -34,15 +43,15 @@ class ResultsPage extends StatelessWidget {
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
                   Text(
-                    'teste',
+                    resultText,
                     style: kNormalResultTextStyle,
                   ),
                   Text(
-                    'teste',
+                    bmiResult,
                     style: kResultNumberStyle,
                   ),
                   Text(
-                    'teste',
+                    interpretation,
                     style: kResultBodyTextStyle,
                   ),
                 ],
